9713a7b375aa81e1e474b821addc58ebc4245795
Ivan <jerzabek.ivan@gmail.com>
2022-05-03 23:12:06 +0200

    HW06: Added game over modal, question progress bar

--- homework06/src/components/questions/QuestionContainer.tsx
+++ homework06/src/components/questions/QuestionContainer.tsx
@@ -2,17 +2,43 @@ import { useContext, useState } from 'react'
 import styled from 'styled-components'
 import QuestionContext from '../../context/QuestionContext'
 import { CenteredContainer } from '../../styledComponents/Container'
+import { CustomButton } from './Answer'
 import QuestionComponent from './Question'
 
-const FixedCenteredContainer = styled(CenteredContainer)`
+const Modal = styled(CenteredContainer)`
   position: fixed;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+
+  padding: 3em;
+  color: #000;
+  border-radius: 0.4em;
+  max-width: 50%;
+  max-height: 300px;
+
+  background-image: url('/images/lost.gif');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  display: flex;
+  flex-direction: column;
+`
+
+const ModalTitle = styled.span`
+  background-color: #FFFFFFDD;
+  border-radius: 0.4em;
+  padding: 1em 1.4em;
+`
+
+const ModalButtonContainer = styled.div`
+  max-width: 200px;
 `
 
 function QuestionContainer() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [modal, setModal] = useState<string>()
 
   const questionsContext = useContext(QuestionContext);
 
@@ -27,20 +53,34 @@ function QuestionContainer() {
   }
 
   function failure() {
-    // alert('game over')
+    setModal("You've failed! Game over.")
   }
 
   return (
     <div>
       <p>Current question: {currentQuestion + 1}/{questionsContext.length}</p>
+
+      <svg width="100%" height="1em" style={{ margin: '1em 0' }}>
+        <rect fill="#38b000" x={0} y={0} width={((currentQuestion + 1) / questionsContext.length) * 100 + "%"} height={100} />
+        <rect fillOpacity={0} stroke="#006400" strokeWidth={3} strokeOpacity={1.0} x={0} y={0} width={'100%'} height={'1em'} />
+      </svg>
+
       <QuestionComponent
         correctGuess={nextQuestion}
         wrongGuess={failure}
         question={questionsContext[currentQuestion]} />
       {
-        false && (
-          <FixedCenteredContainer>
-          </FixedCenteredContainer>
+        modal && (
+          <Modal>
+            <ModalTitle>{modal}</ModalTitle>
+
+            <ModalButtonContainer>
+              <CustomButton
+                backgroundColor='#DB3A34'
+                foregroundColor='#FFFFFA'
+                onClick={() => window.location.reload()}>Back</CustomButton>
+            </ModalButtonContainer>
+          </Modal>
         )
       }
     </div>
